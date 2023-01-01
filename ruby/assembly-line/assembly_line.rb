7f832ad6db30e824e171b38006c952f136936135
Itacir Ferreira Pompeu <itacir.pompeu@pier.digital>
2023-01-01 15:40:17 -0300

    resolved assembly line ruby

--- ruby/assembly-line/assembly_line.rb
+++ ruby/assembly-line/assembly_line.rb
@@ -1,13 +1,29 @@
 class AssemblyLine
+  BASE_RATE = 221.0
   def initialize(speed)
     @speed = speed
   end
 
   def production_rate_per_hour
-    raise 'Please implement the AssemblyLine#production_rate_per_hour method'
+    case @speed
+    when 1..4 
+      get_percent
+    when 5..8
+      get_percent(90)
+    when 9
+      get_percent(80)
+    else
+      get_percent(77)
+    end
   end
 
   def working_items_per_minute
-    raise 'Please implement the AssemblyLine#working_items_per_minute method'
+    (production_rate_per_hour / 60).to_i
+  end
+
+  private
+  def get_percent(percent = 100)
+    num = ((@speed * BASE_RATE) * percent) / 100
+    "#{'%.1f' % num}".to_f
   end
 end
